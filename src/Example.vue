7f40415499a81aeb8aacdfdc0e9a260aee50e1d8
Benjamin Ansbach <benjaminansbach@gmail.com>
2020-09-21 23:50:06 +0200

    v0.1.1

--- src/Example.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <!-- First display the usage-code itself. -->
-  <p><strong>Usage</strong></p>
-  <pre><code><slot name="code"></slot></code></pre>
-
-  <!-- Then let vue "execute" the code. -->
-  <p><strong>The Result</strong></p>
-  <slot name="result"></slot>
-</template>
